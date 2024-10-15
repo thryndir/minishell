adf7bfafb7f8e6947e0df8e4b458e85b03dca04d
jgerbaul <jgerbaul@c2r1p6.42lehavre.fr>
2024-10-15 23:03:33 +0200

    add modified split to handle every symbol as separators

---
+++ src/tokenizer/tokenizer_utils3.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_utils3.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 22:54:06 by jgerbaul          #+#    #+#             */
+/*   Updated: 2024/10/15 22:59:58 by jgerbaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * This function extract a word between quotes in the input string.
+*/
+char	*extract_quoted_word(const char *str, int *i, int len)
+{
+	int		k;
+	char	*word;
+	char	quote;
+
+	k = 0;
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (word == NULL)
+		return (NULL);
+	quote = str[*i];
+	word[k++] = str[(*i)++];
+	while (str[*i] && str[*i] != quote)
+		word[k++] = str[(*i)++];
+	if (str[*i])
+		word[k++] = str[(*i)++];
+	word[k] = '\0';
+	return (word);
+}
+
+/**
+ * This function extract a word in the input string.
+*/
+char	*extract_regular_word(const char *str, int *i, int len)
+{
+	int		k;
+	char	*word;
+
+	k = 0;
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (word == NULL)
+		return (NULL);
+	while (k < len)
+		word[k++] = str[(*i)++];
+	word[k] = '\0';
+	return (word);
+}
+
+/**
+ *  This function extract word and use two other function to bypass
+ * the 25 lines limit.
+*/
+char	*extract_word(const char *str, int *i, int *status)
+{
+	int		len;
+	char	*word;
+
+	len = ft_charnbr(str + *i);
+	if (is_quote(str[*i]))
+		word = extract_quoted_word(str, i, len);
+	else
+		word = extract_regular_word(str, i, len);
+	if (word == NULL)
+		*status = -1;
+	return (word);
+}
+
+/**
+ *  This function convert the input string to a **tab.
+*/
+char	**ft_str_to_array(char **strs, const char *str)
+{
+	int	i;
+	int	j;
+	int	status;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] && str[i] == ' ')
+			i++;
+		if (!str[i])
+			break ;
+		status = 0;
+		strs[j] = extract_word(str, &i, &status);
+		if (status == -1)
+		{
+			ft_malloc_fail(strs, j);
+			return (NULL);
+		}
+		j++;
+	}
+	strs[j] = NULL;
+	return (strs);
+}
+
+/**
+ * Modified version of ft_split to help me tokenize the input string.
+*/
+char	**ft_split(char const *str)
+{
+	char	**strs;
+	int		word_count;
+
+	if (str == NULL)
+		return (NULL);
+	word_count = ft_strnbr(str);
+	strs = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (strs == NULL)
+		return (NULL);
+	return (ft_str_to_array(strs, str));
+}
---
+++ src/tokenizer/tokenizer_utlis2.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_utlis2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 22:51:49 by jgerbaul          #+#    #+#             */
+/*   Updated: 2024/10/15 22:59:59 by jgerbaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * Main word counter here, the other two are here because of
+ * the 25 line restriction.
+*/
+int	count_word(const char *str, int *i)
+{
+	if (is_double_symbol(str, *i))
+	{
+		*i += 2;
+		return (1);
+	}
+	if (is_symbol(str[*i]) && !is_quote(str[*i]))
+	{
+		(*i)++;
+		return (1);
+	}
+	if (is_quote(str[*i]))
+		return (count_quoted_word(str, i));
+	if (str[*i] && !is_symbol(str[*i]) && str[*i] != ' ')
+		return (count_regular_word(str, i));
+	return (0);
+}
+
+/**
+ * This function count the number a word in the input string.
+*/
+int	ft_strnbr(const char *str)
+{
+	int	result;
+	int	i;
+
+	result = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] && str[i] == ' ')
+			i++;
+		result += count_word(str, &i);
+	}
+	return (result);
+}
+
+/**
+ * This function give back the number of char before specific
+ * requirements (quote search).
+*/
+int	ft_charnbr(const char *str)
+{
+	int		i;
+	char	quote;
+
+	i = 0;
+	if (is_double_symbol(str, 0))
+		return (2);
+	if (is_symbol(str[0]) && !is_quote(str[0]))
+		return (1);
+	if (is_quote(str[0]))
+	{
+		quote = str[0];
+		i++;
+		while (str[i] && str[i] != quote)
+			i++;
+		return (i + 1);
+	}
+	while (str[i] && !is_symbol(str[i]) && str[i] != ' ')
+		i++;
+	return (i);
+}
+
+/**
+ * This function free every array that was created before a malloc failure.
+*/
+void	ft_malloc_fail(char **strs, int j)
+{
+	int	i;
+
+	i = 0;
+	while (i < j)
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+}
