1caffd8942aa448a3a219638d87978dc09fe73e5
jgerbaul <jgerbaul@c2r1p6.42lehavre.fr>
2024-10-16 22:48:16 +0200

    header updated && syntax function added

--- include/minishell.h
+++ include/minishell.h
@@ -1,4 +1,18 @@
+<<<<<<< HEAD
 
+=======
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 17:34:36 by thryndir          #+#    #+#             */
+/*   Updated: 2024/10/16 22:46:55 by jgerbaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+>>>>>>> b05c83e (header updated && syntax function added)
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -124,4 +138,38 @@ int			name_and_value(char *var, t_env *env);
 void		del_env(t_env *env);
 char		*get_value(t_env *env, char *name);
 
+/**
+ * PARSING FUNCTIONS
+*/
+
+/**
+ * Syntax
+*/
+bool		ft_isspace(char c);
+void		quote_count(char c, int *simple_q, int *double_q);
+bool		is_symbol_or_nullchar(char c);
+bool		check_after_redir(const char **input, char redir);
+bool		misplaced_operators(const char *input);
+bool		check_invalid_redirection(const char *input);
+bool		invalid_operators(const char *input);
+bool		unclosed_quotes(const char *input);
+bool		syntax_errors(const char *input);
+
+/**
+ * Tokenizer
+*/
+bool		is_quote(char c);
+bool		is_symbol(char c);
+bool		is_double_symbol(const char *str, int i);
+bool		count_quoted_word(const char *str, int *i);
+bool		count_regular_word(const char *str, int *i);
+int			count_word(const char *str, int *i);
+int			ft_strnbr(const char *str);
+int			ft_charnbr(const char *str);
+void		ft_malloc_fail(char **strs, int j);
+char		*extract_quoted_word(const char *str, int *i, int len);
+char		*extract_regular_word(const char *str, int *i, int len);
+char		*extract_word(const char *str, int *i, int *status);
+char		**ft_str_to_array(char **strs, const char *str);
+char		**ft_mini_split(char const *str);
 #endif
---
+++ src/syntax_checker/syntax_utils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   syntax_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 22:43:19 by jgerbaul          #+#    #+#             */
+/*   Updated: 2024/10/16 22:44:12 by jgerbaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * This function check the input character to see whether it's a
+ * space (or equivalent character) or something else.
+*/
+bool	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v')
+		return (true);
+	else
+		return (false);
+}
+
+/**
+ * This function act as a quote counter, it take a character in
+ * entry and check whether it's a quote or a double quote and increment
+ * simple_q or double_q according to that.
+*/
+void	quote_count(char c, int *simple_q, int *double_q)
+{
+	if (c == '\"')
+		*double_q++;
+	if (c == '\'')
+		*simple_q++;
+}
+
+/**
+ * This function check if the current char is a
+ * redir pipe or null character.
+*/
+bool	is_symbol_or_nullchar(char c)
+{
+	return (c == '<' || c == '>' || c == '|' || c == '\0');
+}
+
+/**
+ * This function is used when encountering a redir < or >
+ * and check if the following character is the same redir
+ * or another symbol.
+*/
+bool	check_after_redir(const char **input, char redir)
+{
+	if (**input == redir)
+		(*input)++;
+	while (**input && ft_isspace(**input))
+		(*input)++;
+	return (is_symbol_or_nullchar(**input));
+}
