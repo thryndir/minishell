b05c83e48ea642cfb94126b8a629a916d84d0157
jgerbaul <jgerbaul@c2r1p6.42lehavre.fr>
2024-10-16 22:48:16 +0200

    header updated && syntax function added

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgalloux <lgalloux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 17:34:36 by thryndir          #+#    #+#             */
-/*   Updated: 2024/09/24 18:41:53 by lgalloux         ###   ########.fr       */
+/*   Updated: 2024/10/16 22:46:55 by jgerbaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,4 +122,38 @@ int			export_builtin(t_pipex *pipex);
 int			pwd_builtin(t_pipex *pipex);
 int			unset_builtin(t_pipex *pipex);
 
+/**
+ * PARSING FUNCTIONS
+*/
+
+/**
+ * Syntax
+*/
+bool		ft_isspace(char c);
+void		quote_count(char c, int *simple_q, int *double_q);
+bool		is_symbol_or_nullchar(char c);
+bool		check_after_redir(const char **input, char redir);
+bool		misplaced_operators(const char *input);
+bool		check_invalid_redirection(const char *input);
+bool		invalid_operators(const char *input);
+bool		unclosed_quotes(const char *input);
+bool		syntax_errors(const char *input);
+
+/**
+ * Tokenizer
+*/
+bool		is_quote(char c);
+bool		is_symbol(char c);
+bool		is_double_symbol(const char *str, int i);
+bool		count_quoted_word(const char *str, int *i);
+bool		count_regular_word(const char *str, int *i);
+int			count_word(const char *str, int *i);
+int			ft_strnbr(const char *str);
+int			ft_charnbr(const char *str);
+void		ft_malloc_fail(char **strs, int j);
+char		*extract_quoted_word(const char *str, int *i, int len);
+char		*extract_regular_word(const char *str, int *i, int len);
+char		*extract_word(const char *str, int *i, int *status);
+char		**ft_str_to_array(char **strs, const char *str);
+char		**ft_mini_split(char const *str);
 #endif
