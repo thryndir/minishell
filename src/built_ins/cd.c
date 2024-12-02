6fac7ada5a8b40c32ba6600af5f886b1d5354f89
oui-z <133750887+oui-z@users.noreply.github.com>
2024-12-03 00:52:49 +0100

    fixed four minor problems

--- src/built_ins/cd.c
+++ src/built_ins/cd.c
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgalloux <lgalloux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:49:45 by lgalloux          #+#    #+#             */
-/*   Updated: 2024/11/20 11:49:46 by lgalloux         ###   ########.fr       */
+/*   Updated: 2024/12/03 00:51:46 by jgerbaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executing.h"
+
+int	is_forbidden_symbol(char *str)
+{
+	if (!ft_strcmp(str, ".."))
+		return (1);
+	else if (!ft_strcmp(str, "~"))
+		return (1);
+	else if (!ft_strcmp(str, "-"))
+		return (1);
+	else
+		return (0);
+}
 
 int	cd_builtin(t_command *cmd, t_exec *exec)
 {
@@ -18,6 +30,11 @@ int	cd_builtin(t_command *cmd, t_exec *exec)
 	int		to_free;
 
 	to_free = 0;
+	if (chdir(cmd->argv[1]) != 0 || is_forbidden_symbol(cmd->argv[1]))
+	{
+		printf("cd: No such file or directory\n");
+		return (1);
+	}
 	if (cmd->argv[1] && (cmd->argv[1][0] == '~'
 		|| !ft_strncmp("/home", cmd->argv[1], 5)))
 		path = cmd->argv[1];
@@ -27,7 +44,6 @@ int	cd_builtin(t_command *cmd, t_exec *exec)
 				"/", cmd->argv[1]);
 		to_free = 1;
 	}
-	chdir(cmd->argv[1]);
 	add_in_env(exec->env, "PWD", path);
 	if (to_free)
 		gc_free(path);
