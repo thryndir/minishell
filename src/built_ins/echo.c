e71ce9a9bdac9c65ad10c126a021ee2825751fe7
oui-z <133750887+oui-z@users.noreply.github.com>
2024-12-05 02:05:41 +0100

    echo modified to avoid bad memory access in case of echo followed by a single letter

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -6,25 +6,12 @@
 /*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:49:57 by lgalloux          #+#    #+#             */
-/*   Updated: 2024/12/05 00:22:18 by jgerbaul         ###   ########.fr       */
+/*   Updated: 2024/12/05 02:01:53 by jgerbaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executing.h"
-
-int	is_only_c(char *str, char c)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] != c)
-			return (0);
-		i++;
-	}
-	return (1);
-}
+#include "parsing.h"
 
 int	echo_builtin(t_command *cmd, t_exec *exec)
 {
@@ -34,11 +21,11 @@ int	echo_builtin(t_command *cmd, t_exec *exec)
 	i = 1;
 	(void)exec;
 	newline = 1;
-	if (cmd->argv[i] && is_only_c(cmd->argv[i] + 1, 'n'))
+	if (cmd->argv[i] && !ft_strcmp(cmd->argv[i], "-n"))
 	{
 		newline = 0;
 		i = 2;
-		while (is_only_c(cmd->argv[i] + 1, 'n'))
+		while (!ft_strcmp(cmd->argv[i], "-n"))
 			i++;
 	}
 	while (cmd->argv[i])
