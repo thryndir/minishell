2d92c1bbaa611bce876e8adfac05478429a2b76d
oui-z <133750887+oui-z@users.noreply.github.com>
2024-12-10 11:07:39 +0100

    main update under 25 lines

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thryndir <thryndir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jgerbaul <jgerbaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 20:44:38 by lgalloux          #+#    #+#             */
-/*   Updated: 2024/12/09 13:42:17 by thryndir         ###   ########.fr       */
+/*   Updated: 2024/12/10 11:00:24 by jgerbaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,12 +76,23 @@ void	print_command(t_command *cmd)
 	}
 }
 
+void	main_functions(t_exec *exec, char **tab, char *str)
+{
+	tab = ft_mini_split(str);
+	loop_env_swapper(tab, exec->env);
+	exec->cmd = parse_input(tab);
+	loop_quote_handler(exec->cmd);
+	struct_init(exec, exec->cmd);
+	parent(exec->cmd, exec, 0);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	t_exec	exec;
 	char	*input;
 	char	**splitted_input;
 
+	splitted_input = NULL;
 	(void)argc;
 	(void)argv;
 	set_signal();
@@ -90,17 +101,15 @@ int	main(int argc, char **argv, char **env)
 	{
 		input = readline(">> ");
 		if (!input)
+		{
+			gc_free_all();
 			return (0);
+		}
 		add_history(input);
 		if (syntax_errors(input))
 			continue ;
-		splitted_input = ft_mini_split(input);
-		loop_env_swapper(splitted_input, exec.env);
-		exec.cmd = parse_input(splitted_input);
-		loop_quote_handler(exec.cmd);
-		struct_init(&exec, exec.cmd);
+		main_functions(&exec, splitted_input, input);
 		print_command(exec.cmd);
-		parent(exec.cmd, &exec, 0);
 		main_free_function(&exec, splitted_input, input);
 	}
 	free_env(exec.env);
